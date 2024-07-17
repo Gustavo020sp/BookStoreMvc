f8b78380a3b6f08b498813ba257650af17a63ef8
Gustavo Gomes <gustavo020sp@gmail.com>
2024-07-16 22:07:14 -0300

    Created Author Model

---
+++ BookMvc/Models/Autor.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookMvc.Models
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? ProfilePictureURL { get; set; }
+        public int FullName { get; set; }
+        public int Bio { get; set; }
+
+    }
+}
